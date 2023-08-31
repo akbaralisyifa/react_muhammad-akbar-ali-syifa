5c79fb1567e31b1688d8101e409fc2d3e26361d4
Muhammad Akbar Ali Syifa <akbaralisyifa22@gmail.com>
2023-08-31 11:10:01 +0700

    Membuat Tugas Ke 12

--- 12_Handling/praktikum/src/components/FormPage.jsx
+++ 12_Handling/praktikum/src/components/FormPage.jsx
@@ -1,11 +1,50 @@
-const { Component } = require("react");
-
+import { Component } from "react";
 
 class FormPage extends Component{
     constructor(props){
         super(props)
         this.state = {
+            nama : "",
+            Kategori : "Jacket",
+            image : "img.jpg",
+            freshness : "New Brand",
+            deskripsi: "",
+            price : "",
+        }
 
+    }
+    onchange = e => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const formIsNotEmpty = this.state.nama && this.state.Kategori && this.state.image && this.state.freshness && this.state.deskripsi && this.state.price;
+        if(formIsNotEmpty) {
+            const newData = {
+                nama : this.state.nama,
+                Kategori : this.state.Kategori,
+                image : this.state.image,
+                freshness : this.state.freshness,
+                deskripsi: this.state.deskripsi,
+                price : this.state.price,
+            }
+
+            this.props.tambahProduk(newData);
+
+            this.setState({
+                nama : "",
+                Kategori : "Jacket",
+                image : "img.jpg",
+                freshness : "New Brand",
+                deskripsi: "",
+                price : "",
+            })
+        }else{
+            alert("Please enter a valid product name.")
         }
     }
 
@@ -15,12 +54,12 @@ class FormPage extends Component{
                 <div onSubmit={() => {}} className="product-form">
                     <div class="product-form__name">
                         <label for="produkName" class="form-label">Product Name</label>
-                        <input type="text" id="produkName" name=""  />
+                        <input type="text" id="produkName" name="nama" value={this.state.nama} onChange={this.onchange} />
                     </div>
 
                     <div className="product-form__category">
                         <label for="ProductCategory">Product Category</label>
-                        <select id="productCategory">
+                        <select id="productCategory"  name="Kategori" value={this.state.Kategori} onChange={this.onchange}>
                             <option selected>Choose...</option>
                             <option value="Shirt">Shirt</option>
                             <option value="T-Shirt">T-Shirt</option>
@@ -30,7 +69,7 @@ class FormPage extends Component{
 
                     <div class="product-form__image">
                         <label for="formFile" class="form-label">Image Of Product</label>
-                        <input type="file" id="formFile" />
+                        <input type="file" id="formFile" name="image" onChange={this.onchange} />
                     </div>
 
                     <div className="product-form__freshness">
@@ -52,18 +91,17 @@ class FormPage extends Component{
                     
                     <div class="product-form__description">
                         <label for="deskripsi">Additional Description</label>
-                        <textarea id="deskripsi" />
+                        <textarea id="deskripsi" name="deskripsi" value={this.state.deskripsi} onChange={this.onchange} />
                     </div>
 
                     <div class="product-form__price">
                         <label for="price">Product Price</label>
-                        <input type="text" id="price" placeholder="$ 1"  />
+                        <input type="text" id="price" placeholder="$ 1" name="price" value={this.state.price} onChange={this.onchange} />
                     </div>
 
                     <div class="product-form__button">
-                        <button type="submit" class="button">Submit</button>
+                        <button type="submit" class="button" onClick={this.handleSubmit }>Submit</button>
                     </div>
-                    
                 </div>
             </>
         )
--- 12_Handling/praktikum/src/components/Home.jsx
+++ 12_Handling/praktikum/src/components/Home.jsx
@@ -4,13 +4,67 @@ import HeaderPage from "./HeaderPage";
 import { article } from "../article";
 import FormPage from "./FormPage";
 import TabelPage from "./TabelPage";
+import { v4 as uuidv4 } from 'uuid';
 
 class Home extends Component{
     constructor(props){
         super(props)
         this.state = {
-
+            data : [
+                {
+                    id : uuidv4() ,
+                    nama : "Erigo",
+                    Kategori : "Jacket",
+                    image : "img.jpg",
+                    freshness : "New Brand",
+                    deskripsi: "suisusius",
+                    price : "2",
+                },
+                {
+                    id : uuidv4() ,
+                    nama : "Erigo",
+                    Kategori : "Jacket",
+                    image : "img.jpg",
+                    freshness : "New Brand",
+                    deskripsi: "suisusius",
+                    price : "2",
+                },
+                {
+                    id : uuidv4() ,
+                    nama : "Erigo",
+                    Kategori : "Jacket",
+                    image : "img.jpg",
+                    freshness : "New Brand",
+                    deskripsi: "suisusius",
+                    price : "2",
+                },
+                {
+                    id : uuidv4() ,
+                    nama : "Erigo",
+                    Kategori : "Jacket",
+                    image : "img.jpg",
+                    freshness : "New Brand",
+                    deskripsi: "suisusius",
+                    price : "2",
+                },
+            ],
         }
+    }
+
+
+    hapusProduk = (id) => {
+        const newListProduk = this.state.data.filter((produk) => produk.id !== id);
+
+        this.setState({ data : newListProduk})
+    }
+
+    tambahProduk = (produkBaru) => {
+        const newProduk = { id : uuidv4(), ...produkBaru};
+
+        console.log(newProduk.id);
+
+        this.setState({data : [...this.state.data, newProduk]});
+
     }
 
 
@@ -19,8 +73,8 @@ class Home extends Component{
             <>
             <NavbarPage />
             <HeaderPage article={article}/>
-            <FormPage />
-            <TabelPage />
+            <FormPage tambahProduk={this.tambahProduk} />
+            <TabelPage datas={this.state.data} hapusProduk={this.hapusProduk}/>
             </>
         )
     }
