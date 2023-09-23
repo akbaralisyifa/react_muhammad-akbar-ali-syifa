979b2c3b9503194103908ba6a544969729e239bf
Muhammad Akbar Ali Syifa <80014998+akbaralisyifa@users.noreply.github.com>
2023-09-23 09:37:10 +0700

    Menyelesaikan tugas ke 16 pada Global State

---
+++ 16_Global-State/praktikum/src/components/landingPage/Main.jsx
@@ -0,0 +1,18 @@
+
+
+const Main = () => {
+    return (
+        <main>
+            <div className="main-content">
+            <h3>Join Our Newsletter</h3>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
+            <form className="form-landingPage" >
+                <input className="input-landingPage" type="text" />
+                <button className="button-landingPage">Subscribe</button>
+            </form>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
---
+++ 16_Global-State/praktikum/src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+// CSS
+import './assets/style.css';
+import { persistor, store } from './store/store.jsx';
+import { PersistGate } from 'redux-persist/integration/react';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
