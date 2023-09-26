736fd4b0e19d5493a91afce94b79d40db583ce3b
Muhammad Akbar Ali Syifa <80014998+akbaralisyifa@users.noreply.github.com>
2023-09-26 12:08:23 +0700

    Membuat button toggle pada header di file createProduct

--- 18_Restfull-API/praktikum/src/components/createProduct/HeaderProduct.jsx
+++ 18_Restfull-API/praktikum/src/components/createProduct/HeaderProduct.jsx
@@ -1,20 +1,19 @@
+import { useState } from 'react';
 import LogoProduct from './LogoProduct';
+import ToggleButton from './ToogleButton';
+import { article } from '../../article';
 
-const ProductHeader = (props) => {
-  const { article } = props;
+const ProductHeader = () => {
+  // const { article } = props;
 
-  const titles = [article.title];
-  const description = [article.description];
+  const [isChecked, setIsChecked] = useState(true);
 
   return (
     <div className="product-header">
       <LogoProduct />
-      {titles.map((title) => (
-        <h2>{title.en}</h2>
-      ))}
-      {description.map((desc) => (
-        <p>{desc.en}</p>
-      ))}
+      <h2>{isChecked ? article.title.en : article.title.id}</h2>
+      <p>{isChecked ? article.description.en : article.description.id}</p>
+      <ToggleButton isChecked={isChecked} setIsChecked={setIsChecked} />
     </div>
   );
 };
